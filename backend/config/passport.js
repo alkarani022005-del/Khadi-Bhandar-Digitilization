acae53f70a26f414cb3eb2cf9a01187253347b61
Alka Rani <alkarani022005@gmail.com>
2026-04-01 10:34:08 +0530

    fix google callback absolute url

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -6,7 +6,7 @@ const jwt            = require('jsonwebtoken');
 passport.use(new GoogleStrategy({
   clientID:     process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL:  '/api/auth/google/callback',
+  callbackURL: 'https://khadi-bhandar-digitilization.onrender.com/api/auth/google/callback',
   passReqToCallback: true,
 }, async (req, accessToken, refreshToken, profile, done) => {
   try {
